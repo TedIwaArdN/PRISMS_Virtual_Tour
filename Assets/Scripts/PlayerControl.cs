5ebd5e1a74a66bf400941927e7172b1747917e31
TedIwaArdN <edwardtian619@gmail.com>
2022-10-24 14:30:33 -0400

    New Table Build

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,11 +7,15 @@ public class PlayerControl : MonoBehaviour
     public float movingSpeed;
     public float horizontalInput;
     public float verticalInput;
+    public float jumpForce;
+
+    private Rigidbody playerRigi;
+    private bool isOnGround;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRigi = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,5 +25,32 @@ public class PlayerControl : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(horizontalInput * movingSpeed * Time.deltaTime, 0, verticalInput * movingSpeed * Time.deltaTime);
 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            Jump();
+        }
+
+    }
+
+    void Jump() {
+        playerRigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isOnGround = false;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isOnGround = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isOnGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isOnGround = false;
+        }
     }
 }
