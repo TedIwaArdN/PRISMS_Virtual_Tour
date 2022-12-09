9fb676865d9ad2661fddc778708b599a44b8f5d2
TedIwaArdN <edwardtian619@gmail.com>
2022-12-09 12:59:28 -0500

    Change Scene update

--- Assets/Scripts/JumpToEvent.cs
+++ Assets/Scripts/JumpToEvent.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JumpToEvent : MonoBehaviour
 {
     private bool reachedEventArea;
+    public int nextSceneIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class JumpToEvent : MonoBehaviour
         if (reachedEventArea) {
             if (Input.GetKeyDown(KeyCode.Return)) {
                 Debug.Log("Event");
+                SceneManager.LoadScene(nextSceneIndex, LoadSceneMode.Single);
             }
         }
     }
